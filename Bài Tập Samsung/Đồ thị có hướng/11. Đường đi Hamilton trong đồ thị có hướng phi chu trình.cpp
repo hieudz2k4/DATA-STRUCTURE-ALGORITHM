1b21b832148ae94a89a2d4f60e0dd88b7c3a5c78
dataman <hieudz20041999@gmail.com>
2023-11-29 00:43:23 +0700

    update

---
+++ Bài Tập Samsung/Đồ thị có hướng/11. Đường đi Hamilton trong đồ thị có hướng phi chu trình.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+#define MAX_V 10001
+using namespace std;
+
+int v, e;
+vector<int> adj[MAX_V];
+bool visited[MAX_V];
+stack<int> topoSort;
+
+void dfs(int u) {
+    visited[u] = true;
+    for (int adjU : adj[u]) {
+        if (visited[adjU]) break;
+        if (!visited[adjU]) {
+            dfs(adjU);
+        }
+    }
+    topoSort.push(u);
+}
+
+void topologicalSort() {
+    for (int i = 0; i < v; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+bool check() {
+    if (!v || !e) return false;
+
+    topologicalSort();
+    int cnt = 0;
+    while (!topoSort.empty()) {
+        int u = topoSort.top();
+        topoSort.pop();
+        for (int adjU : adj[u]) {
+            if (adjU == topoSort.top()) cnt++;
+        }
+    }
+    return cnt == v-1;
+}
+
+int main() {
+    cin >> v >> e;
+
+    for (int i = 0; i < e; i++) {
+        int k, q;
+        cin >> k >> q;
+        adj[k].push_back(q);
+    }
+
+    if (check()) {
+        cout << "Đồ thị có đường đi Hamilton" << endl;
+    } else {
+        cout << "Đồ thị không có đường đi Hamilton" << endl;
+    }
+
+    return 0;
+}
